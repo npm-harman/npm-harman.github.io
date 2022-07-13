195a8c67d4d9d498a2e87e852290d3abea811ea9
Harman Singh <harmansi@buffalo.edu>
2022-07-13 00:05:57 -0400

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: ':domain',
+    loadChildren: () =>
+      import('./layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: ':domain/not-found',
+    loadChildren: () =>
+      import('./not-found/not-found.module').then((m) => m.NotFoundModule),
+  },
+  { path: '', redirectTo: 'general', pathMatch: 'full' },
+  { path: '**', redirectTo: 'general/not-found', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
